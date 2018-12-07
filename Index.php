36c3c287656505fe167de52f6c69cb0e80e83ef4
mrfinix <45652063+mrfinix@users.noreply.github.com>
2018-12-07 20:25:58 +0700

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,21 @@
+<html><head><title>ATTACK OF CYBER</title>
+<link href="https://fonts.googleapis.com/css?family=Bungee" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Rock+Salt" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Sedgwick+Ave" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Fredoka+One" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Bangers" rel="stylesheet">
+<style>body{background : 0A0A0A;
+Background-image:  url("https://i.ibb.co/f1XD72y/images.jpg")  !important;
+Background-position:  center !important;
+Background-attachment: scroll !important;
+Background-position:  center !important;
+Background-size:  cover !important; 
+}
+</Style>
+<script>
+alert("ASSALAMUALAIKUM ^-^ !!!");
+</script></head><body bgcolor=black><script type="text/javascript" src="http://htmlfreecodes.com/codes/rain.js"></script><br><div class="error"><font color="Red" size="7" face="Bungee"><center>Hacked By ./MR_FINIX</center></font>
+<br><center><img src="http://i63.tinypic.com/2vn5gna.png" width="770" height="670"></div><font color=Red><br><b><font face="Sedgwick Ave" size='6'><center>the internet is our weapon !!<font color=white><br><b><font face="Sedgwick Ave" size='6'><center>connection is our power !!<font color=Red><br><b><font face="Sedgwick Ave" size='6'><center>There Will Never Be A System That Is Safe !!<font color=white><br><b><font face="Sedgwick Ave" size='6'><center>We Are Attack Of Cyber<font color=Red><br><b><font face="Sedgwick Ave" size='6'><center>Exspect Us!</center><marquee behavior="scroll" direction="left" scrollamount="30" scrolldelay="20" width="100%"><font color="orange">__________________________________________________________________</font></marquee><div style="text-shadow: 0px 0px 10px green;"><span style="color: white;"><font face="Bangers"><b>Thanks To : <marquee scrollamount="5" direction="left" width="50%"><span style="color: red;"><font face="Bangers"><b>| ./MR_FINIX | Systemerror | Ghz7 | King_work | cyber_crime | Sandalkiri | Baba Yaga#2 | Root@m3e.x..!?./# | Mr.? | Mr.Kud3T | ./Marchia | MR./4LuL | Mik3eymoRa | InYourDfv47 | Raflz | M®.50H1B_ | 1500M | Dona pelita Lestari | <span style="color: red;"></b></marquee></font></div><script type="text/rocketscript">/*<![CDATA[*/new TypingText(document.getElementById("message"), 90, fypingText.runAll();/*]]>*/</script><marquee behavior="scroll" direction="right" scrollamount="30" scrolldelay="20" width="100%"><font color="orange">_______________________________________________________________________</font></marquee>
+<iframe width="0" height="0" src="http://api.soundcloud.com/tracks/539364423/stream?client_id=a3e059563d7fd3372b49b37f00a00bcf"></iframe>
+</body>
+</html>
